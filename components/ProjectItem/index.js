489dc81d9051cfc82a65b55c664d341ea2493c1c
JD Willemse <fuzzyvagina@gmail.com>
2020-10-27 12:31:50 +0100

    fix: change masonry setup to respect list order

--- components/ProjectItem/index.js
+++ components/ProjectItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 import css from './styles.module.scss'
 import { asText } from '../../utils/prismic'
--- components/ProjectList/index.js
+++ components/ProjectList/index.js
@@ -1,17 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 // import PropTypes from 'prop-types'
-import Masonry from 'react-masonry-css'
 import { get } from 'lodash'
 import { useRouter } from 'next/router'
 
 import css from './styles.module.scss'
 import ProjectItem from '../ProjectItem'
 
+// Masonry strategy from https://bit.ly/3dVysYj
 const ProjectList = ({ allProjects = [] }) => {
   const [list, setList] = useState(allProjects)
   const router = useRouter()
+  const gridRef = useRef()
+  const allItems = useRef()
+  const rowHeight = useRef()
+  const rowGap = useRef()
   const { client } = router.query
 
+  // calculate grid item height and apply this to item
+  function resizeGridItem(item) {
+    const rowSpan = Math.ceil(
+      (item.firstChild.getBoundingClientRect().height + rowGap.current) /
+        (rowHeight.current + rowGap.current)
+    )
+    item.style.gridRowEnd = 'span ' + rowSpan
+  }
+
+  // loop over grid items and calculate size for each
+  function resizeAllGridItems() {
+    // matches media query where multicolumn grid kicks in
+    const mediaQuery = window.matchMedia('(min-width: 768px)')
+    // only start calculating grid when more than one column
+    if (!mediaQuery.matches) {
+      return null
+    }
+
+    const gridComputedStyle = window.getComputedStyle(gridRef.current)
+    rowHeight.current = parseInt(
+      gridComputedStyle.getPropertyValue('grid-auto-rows')
+    )
+    rowGap.current = parseInt(
+      gridComputedStyle.getPropertyValue('grid-row-gap')
+    )
+    for (let item of allItems.current) {
+      resizeGridItem(item)
+    }
+  }
+
+  // filter list of grid items based on client
   useEffect(() => {
     if (!client) {
       setList(allProjects)
@@ -24,23 +59,30 @@ const ProjectList = ({ allProjects = [] }) => {
     }
   }, [allProjects, client])
 
+  // cache grid item list and add window resize listener
+  useEffect(() => {
+    allItems.current = gridRef.current.children
+    resizeAllGridItems()
+    window.addEventListener('resize', resizeAllGridItems)
+
+    return () => {
+      window.removeEventListener('resize', resizeAllGridItems)
+    }
+  }, [])
+
   return (
-    <section className={css.wrap}>
-      <Masonry
-        breakpointCols={3}
-        className={css.grid}
-        columnClassName={css.column}
-      >
-        {list.map((project) => (
-          <ProjectItem {...project} key={project.node._meta.id} />
-        ))}
-      </Masonry>
+    <section className={css.wrap} onLoad={resizeAllGridItems}>
+      <div className={css.masonryWrapper}>
+        <div className={css.masonry} ref={gridRef}>
+          {list.map((project) => (
+            <div key={project.node._meta.id}>
+              <ProjectItem {...project} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   )
-}
-
-ProjectList.propTypes = {
-  // children: PropTypes.node.isRequired,
 }
 
 export default ProjectList
